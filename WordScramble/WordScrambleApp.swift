bb54eb0dbf4f9059ae1d7781be757d71b8401184
Rohit Patidar <patidar_rohit@yahoo.co.in>
2022-06-30 16:33:26 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Rohit Patidar on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
